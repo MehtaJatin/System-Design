42e0fcace8c52fee79ecdea8c5d1820b2b730b3e
MehtaJatin <mehtajatin47.jm@gmail.com>
2025-08-06 00:06:01 +0530

    Leetcode daily question 05-08-2025

---
+++ src/main/java/org/dailyLeetcode/FruitsIntoBasketsII.java
@@ -0,0 +1,19 @@
+package org.dailyLeetcode;
+
+public class FruitsIntoBasketsII {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int count = 0;
+        for (int fruit : fruits) {
+            boolean found = false;
+            for (int i = 0;i<baskets.length;i++) {
+                if (baskets[i] >= fruit) {
+                    baskets[i] = 0;
+                    found = true;
+                    break;
+                }
+            }
+            count += (found ? 0 : 1);
+        }
+        return count;
+    }
+}
